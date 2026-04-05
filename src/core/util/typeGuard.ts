c13eb3a5cbf4fef4b7e4110431b096470f9c1d01
matthewrackley <matthew.rackley@pornova18.com>
2026-04-05 14:07:45 -0500

    Made typeguards cleaner

--- src/core/util/typeGuard.ts
+++ src/core/util/typeGuard.ts
@@ -20,35 +20,57 @@ export const isBasePrimitive = <
       "string", "number", "bigint", "boolean", "symbol", "undefined", "object", "function", "array", "null", "array-like", "union", "xor"
     ].includes(input) && (callback ? callback(input, ...args) : true);
 
-export const isIssueList = <T> (input: unknown[]): input is Issue[] => {
-  return input.every((item) => isIssue(item));
+export const isIssueList = (input: unknown): input is Issue[] => {
+  return Array.isArray(input) && input.every((item) => isIssue(item));
 };
-export const isIssue = <T> (input: unknown): input is Issue => {
+export const isIssue = (input: unknown): input is Issue => {
   return (
     typeof input === 'object' && input !== null &&
-    'path' in input && 'code' in input && 'expected' in input && 'message' in input
+    'path' in input && 'code' in input && 'expected' in input && 'message' in input && 'type' in input
   ) && (
       typeof input.code === 'string' && typeof input.message === 'string' && (
       typeof input.expected === "string" || typeof input.expected === "number"
     )
   );
 }
-export const isArrayIssueList = <T> (input: unknown[]): input is ArrayIssue[] => {
+
+export const isArray = <T = unknown>(input: unknown): input is Array<T> => {
+  return Array.isArray(input) && input.length >= 0;
+}
+
+export const isBasicIssueList = (input: unknown[]): input is BasicIssue[] => {
+  return input.every((item) => isBasicIssue(item));
+}
+export const isArrayIssueList = (input: unknown[]): input is ArrayIssue[] => {
   return input.every((item) => isArrayIssue(item));
 }
-export const isArrayIssue = <T>(input: unknown): input is ArrayIssue => {
-  return isIssue(input) && 'position' in input && typeof input.position === 'number';
+export const isBasicIssue = (input: unknown): input is BasicIssue => {
+  return isIssue(input) && !('position' in input) && input.type === 'basic' && isArrayIssue(input) === false;
+}
+export const isArrayIssue = (input: unknown): input is ArrayIssue => {
+  return isIssue(input) && 'position' in input && typeof input.position === 'number' && input.type === 'array' && isBasicIssue(input) === false;
 }
 export const isValidationResult = <T> (input: unknown): input is ValidationResult<T> => {
   return typeof input === 'object' && input !== null && 'isValid' in input && typeof input.isValid === 'boolean' && (input.isValid ? 'output' in input : 'issues' in input);
 
 }
-export const isValidationSuccess = <T> (input: unknown): input is ValidationSuccess<T> => {
+export const isValidationSuccess = <T>(input: unknown): input is ValidationSuccess<T> => {
   return isValidationResult(input) && input.isValid === true && 'output' in input;
 }
-export const isValidationFailure = <T>(input: unknown): input is ValidationFailure => {
+export const isValidationFailure = (input: unknown): input is ValidationFailure => {
   return isValidationResult(input) && input.isValid === false && 'issues' in input && isIssueList(input.issues);
 }
+export const isRecord = (input: unknown): input is Record<string, unknown> => {
+  return typeof input === 'object' && input !== null && !Array.isArray(input) && Object.keys(input).some(k => typeof k === 'string');
+}
+export const isSchemaNode = (input: unknown): input is SchemaNode => {
+  return input instanceof BaseValidator || isObjectSchema(input);
+}
+export const isObjectSchema = (input: unknown): input is ObjectSchema => {
+  return isRecord(input) && Object.keys(input).some((key) => input[key] instanceof BaseValidator || isObjectSchema(input[key]));
+}
+
+
 export const isInputValidator = <BP extends BasePrimitive, T extends BaseValidator<unknown>> (input: BasePrimitive, instance: BaseValidator<PrimitiveToType<BP>>) => {
   switch (input) {
     case "string":
@@ -76,30 +98,12 @@ export const isInputValidator = <BP extends BasePrimitive, T extends BaseValidat
     case "union":
       return new Union([], "union");
     case "xor":
-      return new Union([], "xor");
+      return new UnionValidator([], "xor");
     case "tuple":
       return new TupleValidator();
     default:
       return false;
   }
-}
-
-
-
-export function defineIssue (issue: Issue, code: IssueCode, ...path: string[]) {
-  const messageMap: Record<IssueCode, string> = {
-    invalid_type: `Expected type ${ issue.expected } but received ${ typeof issue.received }`,
-    multiple_valid: `Input is valid against multiple validators, while XOR mode is enabled`,
-    none_valid: `Input is not valid against any validator`,
-    too_short: `Input is too short, expected minimum length of ${ issue.expected } but received length of ${ typeof issue.received === 'string' || Array.isArray(issue.received) ? issue.received.length : 'unknown' }`,
-    too_long: `Input is too long, expected maximum length of ${ issue.expected } but received length of ${ typeof issue.received === 'string' || Array.isArray(issue.received) ? issue.received.length : 'unknown' }`,
-    undefined: `Expected value to be defined, but received undefined`,
-    non_validator_passby: `Non-validator passed to union or xor schema`,
-    invalid_schema: `Invalid schema provided to validator`,
-  };
-  issue.code = code;
-  issue.path = [...issue.path, ...path];
-  issue.message = messageMap[code];
 }
 const typeguards = {
   isBasePrimitive,
@@ -110,6 +114,10 @@ const typeguards = {
   isValidationResult,
   isValidationSuccess,
   isValidationFailure,
+  isRecord,
+  isSchemaNode,
+  isObjectSchema,
+  isInputValidator,
 };
 
 export default typeguards;
